59f361c29a21bc0636d2489a8fe021d7cda2fe6c
Guilherme Ferrari <guilhermefbravin@gmail.com>
2019-02-14 00:25:07 -0200

    implementacao frontend pag web

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const TweetController = require('./controllers/TweetController');
 const LikeController = require('./controllers/LikeController')
-
 const routes = express.Router();
 
 routes.get('/tweets', TweetController.index);
